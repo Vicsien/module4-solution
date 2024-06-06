c36e7bcc3fbb12991f74ccbc86697c9b39d94266
Vicsien <vsien25@gmail.com>
2024-06-06 12:29:00 +0100

    my new update

---
+++ script.js
@@ -0,0 +1,13 @@
+(function (){
+var names = ["Alexander", "Joshua", "Janet", "Magdalene", "Abigail", "Jonah", "Daniel", "Gabriel", "Elizabeth", "Jeremiah"]
+
+for ( var i  = 0; i < names.length; ++i ){
+    
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if ( firstLetter ===  'j'){
+  byeSpeaker.speak(names[i]);
+    } else { helloSpeaker.speak(names[i]);
+ }
+}
+})();
